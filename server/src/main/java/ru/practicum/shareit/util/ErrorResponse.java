3e19b28322126515ff27e8d836c8f73cdc896141
Piroggard <piroggard@yandex.ru>
2023-11-24 20:52:57 +0300

    Фикс ErrorResponse

--- server/src/main/java/ru/practicum/shareit/util/ErrorResponse.java
+++ server/src/main/java/ru/practicum/shareit/util/ErrorResponse.java
@@ -1,19 +1,10 @@
 package ru.practicum.shareit.util;
 
+import lombok.Data;
+
+@Data
 public class ErrorResponse {
     private final String error;
     private final String description;
 
-    public ErrorResponse(String error, String description) {
-        this.error = error;
-        this.description = description;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
